3a6788e7859b8dec8af106c5fea201a262d6fa9a
Alexis ROLLAND <alexis.rolland@univ-tours.fr>
2024-10-27 09:57:50 +0100

    working on the parser

--- include/bttParser.hpp
+++ include/bttParser.hpp
@@ -1,24 +1,45 @@
 #ifndef __BTT_PARSER_HPP__
 #define __BTT_PARSER_HPP__
 
+#include <filesystem>
+#include <print>
+
 /** inclusion of cxxopts argument parser */
 #include "cxxopts.hpp"      
+
+
+class bttParserFileNotFoundException : public std::exception{};
+
+
 
 const   char    PROGRAM_NAME[]="bttManager";;
 const   char    PROGRAM_DESC[]="Blint Test Tag Manager software";
 
-
 class bttParser{
     private:
         cxxopts::Options options{PROGRAM_NAME, PROGRAM_DESC};
+        cxxopts::ParseResult    result;
+
+        int argc;
+        char **argv;
+
+        std::string filename{};
+                
+        void    setFileName(const std::string &fname) noexcept {this->filename = fname;};
+
+        std::size_t count(const std::string &arg) noexcept {return this->result.count(arg);};
+        std::string getStrResult(const std::string &arg) {return this->result[arg].as<std::string>();};
+        
 
     public:
         bttParser() = delete;
         virtual ~bttParser() = default;
 
-        bttParser(int argc, const char *argv[]);
+        bttParser(int argc, char *argv[]);
 
+        std::string getFileName() noexcept {return this->filename;};
 
+        void    processArgs();
 
 };
 
--- src/bttParser.cpp
+++ src/bttParser.cpp
@@ -1,6 +1,33 @@
 #include "bttParser.hpp"
 
 
+bttParser::bttParser(int argc, char *argv[]):argc{argc},argv{argv}{
+
+    cxxopts::value<std::string>()->default_value("");
+
+    this->options.add_options()
+        ("d,dump","Dump all file tags")
+        ("h,help", "Print help")
+        ("filename", "The filename to process", cxxopts::value<std::string>());
+
+    this->options.parse_positional({"filename"});
+    
+    // Parse options the usual way
+    this->result = this->options.parse(this->argc, this->argv);
+
+    // Check file's existence
+    this->setFileName(this->getStrResult("filename"));
+    if (!std::filesystem::exists(this->getFileName())) throw bttParserFileNotFoundException{};
+
+}
+
+void    bttParser::processArgs(){
+    if (result.count("help") > 0) {
+        std::println("{}", options.help());
+        return;
+    }
+}
+
 
 
 
